e7114929053cc5b6e3de2658cf4bbf315e519d01
Silent <zdanio95@gmail.com>
2018-02-19 01:04:18 +0100

    FPS cap adjustable from SilentPatchGF.ini

--- SilentPatchGF/SilentPatchGF.cpp
+++ SilentPatchGF/SilentPatchGF.cpp
@@ -9,8 +9,11 @@
 
 #include "DirectDraw7_RwD3D9.h"
 #include <cassert>
+#include <Shlwapi.h>
 
 #include <rwcore.h>
+
+#pragma comment(lib, "shlwapi.lib")
 
 HINSTANCE hDLLModule;
 
@@ -24,6 +27,26 @@ HRESULT WINAPI DirectDrawRwD3D9Create( GUID *lpGUID, LPVOID *lplpDD, REFIID iid,
 }
 
 
+static void (*orgDelayedHookingPoint)(int);
+void InjectDelayedPatches( int val )
+{
+	orgDelayedHookingPoint( val );
+
+	const HINSTANCE hInstance = GetModuleHandle( nullptr );
+	std::unique_ptr<ScopedUnprotect::Unprotect> Protect = ScopedUnprotect::UnprotectSectionOrFullModule( hInstance, ".text" );
+	ScopedUnprotect::Section Section2( hInstance, ".rdata");
+
+	// Obtain a path to the ASI
+	wchar_t			wcModulePath[MAX_PATH];
+	GetModuleFileNameW(hDLLModule, wcModulePath, _countof(wcModulePath) - 3); // Minus max required space for extension
+	PathRenameExtensionW(wcModulePath, L".ini");
+
+	if ( int INIoption = GetPrivateProfileIntW(L"SilentPatch", L"FPSLimit", -1, wcModulePath); INIoption != -1 )
+	{
+		Memory::Patch<float>(0x7CF398 + 6, INIoption > 0 ? 1000.0f/INIoption : 0.0f );
+	}
+}
+
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved)
 {
 	UNREFERENCED_PARAMETER(hModule);
@@ -35,11 +58,18 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved)
 		{
 			hDLLModule = hModule;
 
-			ScopedUnprotect::Section Section( GetModuleHandle( nullptr ), ".text");
-			ScopedUnprotect::Section Section2( GetModuleHandle( nullptr ), ".rdata");
+			const HINSTANCE hInstance = GetModuleHandle( nullptr );
+			std::unique_ptr<ScopedUnprotect::Unprotect> Protect = ScopedUnprotect::UnprotectSectionOrFullModule( hInstance, ".text" );
+			ScopedUnprotect::Section Section2( hInstance, ".rdata");
+
 			strcpy_s( (char*)0xA8F4D8, 16, "log.log" );
 
+			Memory::ReadCall( 0x446B7A, orgDelayedHookingPoint );
+			Memory::InjectHook( 0x446B7A, InjectDelayedPatches );
+
 			Memory::InjectHook( 0x8C58D6, DirectDrawRwD3D9Create );
+
+			
 
 			InstallRenderQueueHook();
 			//Memory::InjectHook( 0x75AE90, RwCameraShowRaster_DrawOverlay, PATCH_JUMP );
